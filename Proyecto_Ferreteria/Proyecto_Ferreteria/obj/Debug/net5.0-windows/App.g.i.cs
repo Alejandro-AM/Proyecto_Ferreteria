88b9825328fd426e19a909879c99ebca765071f5
Carlos Ardon <ardon7528@gmail.com>
2021-03-22 15:44:19 -0600

    Archivo que crea el programa

--- Proyecto_Ferreteria/Proyecto_Ferreteria/obj/Debug/net5.0-windows/App.g.i.cs
+++ Proyecto_Ferreteria/Proyecto_Ferreteria/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Proyecto_Ferreteria {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Proyecto_Ferreteria;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Proyecto_Ferreteria;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
