cd8a759897db27e03f9bef4512ff18bd834fcac0
Carlos Ardon <ardon7528@gmail.com>
2021-03-22 15:15:30 -0600

    Archivos creados por el programa

--- Proyecto_Ferreteria/Proyecto_Ferreteria/obj/Debug/net5.0-windows/App.g.i.cs
+++ Proyecto_Ferreteria/Proyecto_Ferreteria/obj/Debug/net5.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Proyecto_Ferreteria {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace Proyecto_Ferreteria {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Proyecto_Ferreteria;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Proyecto_Ferreteria;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace Proyecto_Ferreteria {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
         public static void Main() {
             Proyecto_Ferreteria.App app = new Proyecto_Ferreteria.App();
             app.InitializeComponent();
